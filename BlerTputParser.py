20d1c26ef9e45a66178d177942d7b98363c8d06e
Dustin Chen <dustin_chen@compal.com>
2023-08-20 14:31:56 +0800

    fix bug of parsing example\du_stats_22_12_29_22_18_25.txt below DL-SUCC   DL-NEWTX    DL-RETX did not have number

--- BlerTputParser.py
+++ BlerTputParser.py
@@ -25,58 +25,46 @@ def parse_stats(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
 
-        dl_succ, dl_newtx, dl_retx, bler, sch_dl_tpt, ul_tpt = None, None, None, None, None, None
-        dl_stats_match = None
-
         index = 0
 
-        for index, line in enumerate(lines):
-            dl_succ_found = False  # Track whether DL-SUCC information is found for the current entry
-
+        while index < len(lines):
+            line = lines[index]
+            
             if "DL-SUCC   DL-NEWTX    DL-RETX" in line and len(lines) > index + 1:
                 next_line_values = lines[index + 1].split()
-                dl_succ = int(next_line_values[9])
-                dl_newtx = int(next_line_values[10])
-                dl_retx = int(next_line_values[11])
-
-                if dl_succ == 0:
-                    while index < len(lines):
-                        index += 1
-                        if index < len(lines) and "DL-SUCC   DL-NEWTX    DL-RETX" in lines[index]:
-                            break
-                else:
-                    bler = dl_retx / (dl_newtx + dl_retx)
-
-                    while index < len(lines):
-                        if "SCH  DL Tpt" in lines[index]:
-                            sch_dl_tpt_match = re.search(r'SCH\s+DL\s+Tpt\s+:\s+(\d+\.\d+)', lines[index])
-                            if sch_dl_tpt_match:
-                                sch_dl_tpt = float(sch_dl_tpt_match.group(1))
-                                if "UL Tpt" in lines[index]:
-                                    ul_tpt_match = re.search(r'UL\s+Tpt\s+(\d+\.\d+)', lines[index])
-                                    if ul_tpt_match:
-                                        ul_tpt = float(ul_tpt_match.group(1))
-                                        results.append((dl_succ, dl_newtx, dl_retx, bler, sch_dl_tpt, ul_tpt))
-                                        dl_succ_found = True
-                                    break
-                        index += 1
-
-            if dl_succ_found:
-                dl_succ = None
                 
+                if len(next_line_values) >= 12:  # Check if there are enough values
+                    dl_succ = int(next_line_values[9])
+                    dl_newtx = int(next_line_values[10])
+                    dl_retx = int(next_line_values[11])
+
+                    if dl_succ != 0:
+                        bler = dl_retx / (dl_newtx + dl_retx)
+
+                        while index < len(lines):
+                            if "SCH  DL Tpt" in lines[index]:
+                                sch_dl_tpt_match = re.search(r'SCH\s+DL\s+Tpt\s+:\s+(\d+\.\d+)', lines[index])
+                                if sch_dl_tpt_match:
+                                    sch_dl_tpt = float(sch_dl_tpt_match.group(1))
+                                    if "UL Tpt" in lines[index]:
+                                        ul_tpt_match = re.search(r'UL\s+Tpt\s+(\d+\.\d+)', lines[index])
+                                        if ul_tpt_match:
+                                            ul_tpt = float(ul_tpt_match.group(1))
+                                            results.append((dl_succ, dl_newtx, dl_retx, bler, sch_dl_tpt, ul_tpt))
+                                            break
+                            index += 1
+
             index += 1
             
     return results
 
 # Use the glob module to find all files matching the pattern du_stats_*.txt
 file_list = glob.glob("du_stats_*.txt")
-all_results = []
 
 if file_list:
     for filename in file_list:
         results = parse_stats(filename)
         if results:
-            all_results.extend(results)
             for dl_succ, dl_newtx, dl_retx, bler, sch_dl_tpt, ul_tpt in results:
                 if bler is not None:
                     formatted_bler = "{:.5f}".format(bler)
@@ -90,10 +78,3 @@ if file_list:
             print(f"No valid results found in {filename}")
 else:
     print("No matching files found.")
-
-# Print all results after processing all files (optional)
-for dl_succ, dl_newtx, dl_retx, bler, sch_dl_tpt, ul_tpt in all_results:
-    if bler is not None:
-        formatted_bler = "{:.5f}".format(bler)
-    else:
-        formatted_bler = "N/A"
