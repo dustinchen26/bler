a5be05260a31a0222a736a234b6ff47ad4b85341
Dustin Chen <dustin_chen@compal.com>
2023-08-22 19:44:43 +0800

    add UL parse. ul_bler = ul_retx / (ul_newtx + ul_retx)

--- BlerTputParser.py
+++ BlerTputParser.py
@@ -11,12 +11,12 @@ import glob
 import re
 import os
 
-# Check if the file parse_DL_BLER_results.txt exists and delete it if it does
-if os.path.exists("parse_DL_BLER_results.txt"):
-    os.remove("parse_DL_BLER_results.txt")
-    print("Deleted existing parse_DL_BLER_results.txt")
+# Check if the file parse_DL_UL_BLER_results.txt exists and delete it if it does
+if os.path.exists("parse_DL_UL_BLER_results.txt"):
+    os.remove("parse_DL_UL_BLER_results.txt")
+    print("Deleted existing parse_DL_UL_BLER_results.txt")
 else:
-    print("parse_DL_BLER_results.txt does not exist.")
+    print("parse_DL_UL_BLER_results.txt does not exist.")
 
 # Define the function parse_stats to parse statistical data files and return parsed results
 def parse_stats(filename):
@@ -41,18 +41,36 @@ def parse_stats(filename):
                     if dl_succ != 0:
                         dl_bler = dl_retx / (dl_newtx + dl_retx)
 
-                        while index < len(lines):
-                            if "SCH  DL Tpt" in lines[index]:
-                                sch_dl_tpt_match = re.search(r'SCH\s+DL\s+Tpt\s+:\s+(\d+\.\d+)', lines[index])
-                                if sch_dl_tpt_match:
-                                    sch_dl_tpt = float(sch_dl_tpt_match.group(1))
-                                    if "UL Tpt" in lines[index]:
-                                        ul_tpt_match = re.search(r'UL\s+Tpt\s+(\d+\.\d+)', lines[index])
-                                        if ul_tpt_match:
-                                            ul_tpt = float(ul_tpt_match.group(1))
-                                            results.append((dl_succ, dl_newtx, dl_retx, dl_bler, sch_dl_tpt, ul_tpt))
-                                            break
-                            index += 1
+                        # Parsing UL statistics
+                        ul_section_found = False
+                        for ul_index in range(index + 1, len(lines)):
+                            ul_line = lines[ul_index]
+                            if "UL-NEWTX   UL-RETX" in ul_line:
+                                ul_stats = lines[ul_index + 1].split()
+                                if len(ul_stats) >= 10:
+                                    ul_succ = int(ul_stats[7])
+                                    ul_newtx = int(ul_stats[8])
+                                    ul_retx = int(ul_stats[9])
+                                    if ul_succ != 0:
+                                        ul_bler = ul_retx / (ul_newtx + ul_retx)
+                                    else:
+                                        ul_bler = None
+                                    ul_section_found = True
+                                    break
+
+                        if ul_section_found:
+                            while index < len(lines):
+                                if "SCH  DL Tpt" in lines[index]:
+                                    sch_dl_tpt_match = re.search(r'SCH\s+DL\s+Tpt\s+:\s+(\d+\.\d+)', lines[index])
+                                    if sch_dl_tpt_match:
+                                        sch_dl_tpt = float(sch_dl_tpt_match.group(1))
+                                        if "UL Tpt" in lines[index]:
+                                            ul_tpt_match = re.search(r'UL\s+Tpt\s+(\d+\.\d+)', lines[index])
+                                            if ul_tpt_match:
+                                                ul_tpt = float(ul_tpt_match.group(1))
+                                                results.append((dl_succ, dl_newtx, dl_retx, dl_bler, sch_dl_tpt, ul_succ, ul_newtx, ul_retx, ul_bler, ul_tpt))
+                                                break
+                                index += 1
 
             index += 1
             
@@ -65,14 +83,13 @@ if file_list:
     for filename in file_list:
         results = parse_stats(filename)
         if results:
-            for dl_succ, dl_newtx, dl_retx, dl_bler, sch_dl_tpt, ul_tpt in results:
-                if dl_bler is not None:
-                    formatted_dl_bler = "{:.5f}".format(dl_bler)
-                else:
-                    formatted_dl_bler = "N/A"
-                output_line = "DL-SUCC: {:<6} DL-NEWTX: {:<6} DL-RETX: {:<6} DL_BLER: {:<12} SCH  DL Tpt: {:<10} UL Tpt: {:<10}".format(dl_succ, dl_newtx, dl_retx, formatted_dl_bler, sch_dl_tpt, ul_tpt)
+            for dl_succ, dl_newtx, dl_retx, dl_bler, sch_dl_tpt, ul_succ, ul_newtx, ul_retx, ul_bler, ul_tpt in results:
+                formatted_dl_bler = "{:.5f}".format(dl_bler) if dl_bler is not None else "N/A"
+                formatted_ul_bler = "{:.5f}".format(ul_bler) if ul_bler is not None else "N/A"
+                output_line = "DL-SUCC: {:<6} DL-NEWTX: {:<6} DL-RETX: {:<6} DL_BLER: {:<12} SCH  DL Tpt: {:<10} UL-SUCC: {:<6} UL-NEWTX: {:<6} UL-RETX: {:<6} UL_BLER: {:<12} UL Tpt: {:<10}".format(dl_succ, dl_newtx, dl_retx, formatted_dl_bler, sch_dl_tpt, ul_succ, ul_newtx, ul_retx, formatted_ul_bler, ul_tpt)
+                
                 print(output_line)
-                with open("parse_DL_BLER_results.txt", "a") as output_file:
+                with open("parse_DL_UL_BLER_results.txt", "a") as output_file:
                     output_file.write(output_line + "\n")
         else:
             print(f"No valid results found in {filename}")
--- README.md
+++ README.md
@@ -5,15 +5,40 @@ Author: Dustin_Chen  Email:  Dustin_Chen@compal.com or chuhpsdustin@gmail.com
 
 ## How to use
 ```
-Input the du_stats_XXX.txt, then it will parse the ( DL-SUCC, DL-NEWTX, DL-RETX, BLER, SCH  DL Tpt, UL Tpt )  
+Input the du_stats_XXX.txt, then it will parse the ( DL-SUCC, DL-NEWTX, DL-RETX, DL_BLER, SCH  DL Tpt, UL-SUCC, UL-NEWTX, UL-RETX, UL_BLER, UL Tpt)  
 
 Step:
 1.	decompress the BlerTputParser.7z
 2.	Put any type of du_stats_XXX log in the decompressed folder
 3.	Execute the BlerTputParser.exe
-4.	View the calculation results parse_DL_BLER_results.txt
+4.	View the calculation results "parse_DL_UL_BLER_results.txt"
 
 Calculation formula:
-BLER = DL-RETX / (DL-NEWTX +DL-RETX) 
+> dl_bler = dl_retx / (dl_newtx + dl_retx)
+> ul_bler = ul_retx / (ul_newtx + ul_retx)
 ```
 
+## Example
+```
+【Input】"du_stats_22_12_29_22_18_25.txt":
+---------------------------------------------------------------------------------------------
+                       DL Instantaneous Statistics
+---------------------------------------------------------------------------------------------
+CELL-ID   CELL-DL-BANDWIDTH  CELL-UL-BANDWIDTH  AVG-DL-PRB  AVG-DL-PRB-BWP-0  AVG-DL-PRB-BWP-1  AVG-DL-PRB-BWP-2  AVG-DL-PRB-BWP-3  DL-OCC   DL-SUCC   DL-NEWTX    DL-RETX   DL-DTX   DL-NACK-RV[0]   DL-NACK-RV[1]   DL-NACK-RV[2]   DL-NACK-RV[3]   DL-DTX-RV[0]   DL-DTX-RV[1]   DL-DTX-RV[2]   DL-DTX-RV[3]   DL-BLER   
+1         0                  0                  255.10      255.10            0.00              0.00              0.00              48002    25545     25488       57        0        48              1               8               1               0              0              0              0              0         
+
+---------------------------------------------------------------------------------------------
+                       UL Instantaneous Statistics
+---------------------------------------------------------------------------------------------
+CELL-ID   AVG-UL-PRB  AVG-UL-PRB-BWP-0  AVG-UL-PRB-BWP-1  AVG-UL-PRB-BWP-2  AVG-UL-PRB-BWP-3  UL-OCC   UL-SUCC   UL-NEWTX   UL-RETX   UL-DATIND   UL-BLER ERR-PUCCH-NODE ERR-PUCCH-MSGLEN 
+1         24          24                0                 0                 0                 0        6913      6913       0         6217        0       0              0                
+
+Total Number of UEs  : 1
+	 EGTP DL Tpt : 473.91  UL Tpt 0.26
+	 X2 EGTP DL Tpt : 0.00  UL Tpt 0.00
+	 SCH  DL Tpt : 475.50  UL Tpt 0.26	 
+	 
+【Output】"parse_DL_UL_BLER_results.txt"
+DL-SUCC: 25545  DL-NEWTX: 25488  DL-RETX: 57     DL_BLER: 0.00223      SCH  DL Tpt: 475.5      UL-SUCC: 6913   UL-NEWTX: 6913   UL-RETX: 0      UL_BLER: 0.00000      UL Tpt: 0.26
+
+```
